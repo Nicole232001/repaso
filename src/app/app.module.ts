dcba73a856602077cc4b41420c6063992c3b288c
Paola Salas <pacholasalas@gmail.com>
2018-10-11 08:49:24 -0500

    hcer botones y luego al darle click a dos cmbia a pagina on toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { P1Page } from '../pages/p1/p1';
+import { P2Page } from '../pages/p2/p2';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    P1Page,
+    P2Page
+    
   ],
   imports: [
     BrowserModule,
@@ -19,7 +24,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    P1Page,
+    P2Page
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { P1Page } from '../p1/p1';
+import { P2Page } from '../p2/p2';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  page1=P1Page;
+  page2=P2Page;
 
   constructor(public navCtrl: NavController) {
 
   }
-
+  click1(){
+    this.navCtrl.push(this.page1);
+  }
+  click2(){
+    this.navCtrl.push(this.page2);
+  }
 }
